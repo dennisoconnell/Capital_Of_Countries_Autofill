855fb0b3cd3e525912a50108da98b9bf0bd43f05
Dennis O'Connell <doconnell@scu.edu>
2020-04-18 12:48:10 -0700

    created functiion to search and filter capitals.json and another to ouput the HTML
    created functiion to search and filter capitals.json and another to ouput the HTML

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
 const search = document.getElementById("search");
 const matchList = document.getElementById("match-list");
 
+//search capitals.json and filter it
+const searchCountries = async searchText => {
+  const res = await fetch("../data/capitals.json");
+  const countries = await res.json();
+
+  //get matches to current user input
+  let matches = countries.filter(countryName => {
+    const regex = new RegExp(`^${searchText}`, 'gi');
+    return countryName.country.match(regex);
+  });
+
+  if (searchText.length == 0) {
+    matches = [];
+  }
+
+  outputHTML(matches);
+};
+
+//show results in HTML
+const outputHTML = matches => {
+  if (matches.length > 0) {
+    const HTML = matches.map(match => `
+    <div class ="card card-body mb-1"> 
+      <h4>${match.country} (${match.abbr}) <span class = "text-primary" ${match.capital} </span> </h4>
+      <small>Lat: ${match.lat} / Long: ${match.lng}</small>
+    </div>
+    `).join(' ');
+
+    console.log(HTML);
+    matchList.innerHTML = HTML;
+  }
+}
+
 search.addEventListener("input", () => searchCountries(search.value));
